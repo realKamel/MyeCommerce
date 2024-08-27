e47d2c7b977478dc6ac60575cc5b41e064a9e231
Abdelrahman <abdo.aly.k.m@gmail.com>
2024-08-27 16:25:20 +0300

    add some services code

--- src/app/services/brands.service.ts
+++ src/app/services/brands.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { environment } from "../../environments/environment";
+import { Observable } from "rxjs";
 
 @Injectable({
 	providedIn: "root",
 })
 export class BrandsService {
-	constructor() {}
+	private readonly _HttpClient = inject(HttpClient);
+
+	getAllBrands(): Observable<any> {
+		return this._HttpClient.get(`${environment.BaseUrl}/api/v1/brands`);
+	}
+
+	getSpecificBrand(id: string | number): Observable<any> {
+		return this._HttpClient.get(
+			`${environment.BaseUrl}/api/v1/brands/${id}`
+		);
+	}
 }
--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: "root",
 })
 export class CategoriesService {
-
-  constructor() { }
+	private readonly _HttpClient = inject(HttpClient);
+	getAllCategories(): Observable<any> {
+		return this._HttpClient.get(`${environment.BaseUrl}/api/v1/categories`);
+	}
+	getSpecificCategory(id: string | number): Observable<any> {
+		return this._HttpClient.get(
+			`${environment.BaseUrl}/api/v1/categories/${id}`
+		);
+	}
 }
