2712efa96db5d71551bc058842b644637a5fafa3
Abdelrahman <abdo.aly.k.m@gmail.com>
2024-08-28 00:36:02 +0300

    40% of home

--- src/app/components/categorie/categorie.component.ts
+++ src/app/components/categorie/categorie.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from "@angular/core";
+import { CategoriesService } from "../../services/categories.service";
+import { ICategory } from "../../interfaces/icategory";
+import { Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-categorie',
-  standalone: true,
-  imports: [],
-  templateUrl: './categorie.component.html',
-  styleUrl: './categorie.component.scss'
+	selector: "app-categorie",
+	standalone: true,
+	imports: [],
+	templateUrl: "./categorie.component.html",
+	styleUrl: "./categorie.component.scss",
 })
-export class CategorieComponent {
-
+export class CategorieComponent implements OnInit, OnDestroy {
+	_CategoriesService = inject(CategoriesService);
+	AllCategoriesRes!: ICategory[];
+	AllCategoriesSub!: Subscription;
+	ngOnInit(): void {
+		this.AllCategoriesSub = this._CategoriesService
+			.getAllCategories()
+			.subscribe({
+				next: (res) => {
+					this.AllCategoriesRes = res.data;
+				},
+			});
+	}
+	ngOnDestroy(): void {
+		this.AllCategoriesSub?.unsubscribe();
+	}
 }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,18 +4,73 @@ import { Subscription } from "rxjs";
 import { IProduct } from "../../interfaces/iproduct";
 import { NgbRatingModule } from "@ng-bootstrap/ng-bootstrap";
 import { RouterLink } from "@angular/router";
+import { CarouselModule, OwlOptions } from "ngx-owl-carousel-o";
+import { CategoriesService } from "../../services/categories.service";
+import { ICategory } from "../../interfaces/icategory";
 
 @Component({
 	selector: "app-home",
 	standalone: true,
-	imports: [NgbRatingModule, RouterLink],
+	imports: [NgbRatingModule, RouterLink, CarouselModule],
 	templateUrl: "./home.component.html",
 	styleUrl: "./home.component.scss",
 })
 export class HomeComponent implements OnInit, OnDestroy {
 	private readonly _ProductsService = inject(ProductsService);
-	private AllProdSubscribe!: Subscription;
+	private readonly _CategoriesService = inject(CategoriesService);
 	AllProdRes: IProduct[] | null = null;
+	AllCategoriesRes: ICategory[] | null = null;
+	private AllProdSubscribe!: Subscription;
+	private AllCategoriesSub!: Subscription;
+
+	catCustomOptions: OwlOptions = {
+		loop: true,
+		mouseDrag: false,
+		touchDrag: false,
+		pullDrag: false,
+		dots: false,
+		navSpeed: 700,
+		navText: ["", ""],
+		responsive: {
+			0: {
+				items: 1,
+			},
+			400: {
+				items: 2,
+			},
+			740: {
+				items: 4,
+			},
+			940: {
+				items: 6,
+			},
+		},
+		nav: true,
+	};
+	staticCustomOptions: OwlOptions = {
+		loop: true,
+		mouseDrag: false,
+		touchDrag: false,
+		pullDrag: false,
+		dots: false,
+		navSpeed: 700,
+		navText: ["", ""],
+		responsive: {
+			0: {
+				items: 1,
+			},
+			400: {
+				items: 2,
+			},
+			740: {
+				items: 3,
+			},
+			940: {
+				items: 4,
+			},
+		},
+		nav: true,
+	};
 	ngOnInit(): void {
 		this.AllProdSubscribe = this._ProductsService
 			.getAllProducts()
@@ -27,9 +82,20 @@ export class HomeComponent implements OnInit, OnDestroy {
 					console.log(err);
 				},
 			});
+		this.AllCategoriesSub = this._CategoriesService
+			.getAllCategories()
+			.subscribe({
+				next: (res) => {
+					this.AllCategoriesRes = res.data;
+				},
+				error: (err) => {
+					console.log(err);
+				},
+			});
 	}
 
 	ngOnDestroy(): void {
-		this.AllProdSubscribe.unsubscribe();
+		this.AllProdSubscribe?.unsubscribe();
+		this.AllCategoriesSub?.unsubscribe();
 	}
 }
