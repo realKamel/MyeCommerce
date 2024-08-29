21cccd0876145a8969876540ef0a003bb49ffe5e
abdelrahman <abdo.aly.k.m@gmail.com>
2024-08-29 23:36:18 +0300

    50% of register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,21 @@
-import { Component, inject, OnDestroy, OnInit } from "@angular/core";
+import {
+	Component,
+	inject,
+	OnDestroy,
+	OnInit,
+	signal,
+	WritableSignal,
+} from "@angular/core";
 import { AuthService } from "../../services/auth.service";
-import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import {
+	AbstractControl,
+	FormControl,
+	FormGroup,
+	ReactiveFormsModule,
+	Validators,
+} from "@angular/forms";
+import { HttpErrorResponse } from "@angular/common/http";
+import { Subscription } from "rxjs";
 
 @Component({
 	selector: "app-register",
@@ -9,19 +24,60 @@ import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 	templateUrl: "./register.component.html",
 	styleUrl: "./register.component.scss",
 })
-export class RegisterComponent implements OnInit, OnDestroy {
-	private _AuthService = inject(AuthService);
+export class RegisterComponent implements OnDestroy {
+	private _AuthService: AuthService = inject(AuthService);
 
-	registerForm: FormGroup = new FormGroup({
-		name: new FormControl(null),
-		email: new FormControl(null),
-		password: new FormControl(null),
-		rePassword: new FormControl(null),
-		phone: new FormControl(null),
-	});
-	ngOnInit(): void {}
-	test() {
-		console.log("good", this.registerForm.value);
+	private signUpSub!: Subscription;
+	isLoading: WritableSignal<boolean> = signal(false);
+	errMsg: WritableSignal<string> = signal("");
+
+	registerForm: FormGroup = new FormGroup(
+		{
+			name: new FormControl(null, [
+				Validators.required,
+				Validators.minLength(3),
+				Validators.maxLength(20),
+			]),
+			email: new FormControl(null, [
+				Validators.required,
+				Validators.email,
+			]),
+			password: new FormControl(null, [
+				Validators.required,
+				Validators.pattern(/^\w{6,}$/),
+			]),
+			rePassword: new FormControl(null),
+			phone: new FormControl(null, [
+				Validators.required,
+				Validators.pattern(/^01[0125][0-9]{8}$/),
+			]),
+		},
+		this.checkPasswords
+	);
+	checkPasswords(f: AbstractControl) {
+		if (f.get("password")?.value === f.get("rePassword")?.value) {
+			return null;
+		}
+		return { mismatch: "true" };
 	}
-	ngOnDestroy(): void {}
+	submitRegisterForm() {
+		if (this.registerForm.valid) {
+			this.isLoading.set(true);
+			this.signUpSub = this._AuthService
+				.signUp(this.registerForm.value)
+				.subscribe({
+					next: (value) => {
+						console.log(value);
+						this.isLoading.set(false);
+					},
+					error: (error: HttpErrorResponse) => {
+						this.errMsg.set(error.error.message);
+						this.isLoading.set(false);
+					},
+				});
+		}
+	}
+	ngOnDestroy(): void {
+		this.signUpSub?.unsubscribe();
+	}
 }
