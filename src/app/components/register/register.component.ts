b46004a7bed3ca21779e8b75400133024c4a1249
abdelrahman <abdo.aly.k.m@gmail.com>
2024-08-29 20:28:47 +0300

    10% of register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  standalone: true,
-  imports: [],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+	selector: "app-register",
+	standalone: true,
+	imports: [ReactiveFormsModule],
+	templateUrl: "./register.component.html",
+	styleUrl: "./register.component.scss",
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit, OnDestroy {
+	private _AuthService = inject(AuthService);
 
+	registerForm: FormGroup = new FormGroup({
+		name: new FormControl(null),
+		email: new FormControl(null),
+		password: new FormControl(null),
+		rePassword: new FormControl(null),
+		phone: new FormControl(null),
+	});
+	ngOnInit(): void {}
+	test() {
+		console.log("good", this.registerForm.value);
+	}
+	ngOnDestroy(): void {}
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,51 @@
-import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "../../environments/environment";
 
 @Injectable({
 	providedIn: "root",
 })
 export class AuthService {
-	constructor() {}
+	private _HttpClient = inject(HttpClient);
+
+	signUp(userDate: object): Observable<any> {
+		return this._HttpClient.post(
+			`${environment.BaseUrl}/api/v1/auth/signup`,
+			userDate
+		);
+	}
+
+	signIn(userDate: object): Observable<any> {
+		return this._HttpClient.post(
+			`${environment.BaseUrl}/api/v1/auth/signin`,
+			userDate
+		);
+	}
+	forgetPassword(email: string): Observable<any> {
+		return this._HttpClient.post(
+			`${environment.BaseUrl}/api/v1/auth/forgotPasswords`,
+			{
+				email: `${email}`,
+			}
+		);
+	}
+	verifyResetCode(code: string): Observable<any> {
+		return this._HttpClient.post(
+			`${environment.BaseUrl}/api/v1/users/changeMyPassword`,
+			{
+				resetCode: `${code}`,
+			}
+		);
+	}
+
+	resetPassword(email: string, password: string): Observable<any> {
+		return this._HttpClient.put(
+			`${environment.BaseUrl}/api/v1/auth/resetPassword`,
+			{
+				email: `${email}`,
+				newPassword: `${password}`,
+			}
+		);
+	}
 }
